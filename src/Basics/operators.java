6cc58d2403b191ede331775665980c18f9320017
Abhishek <abhishek.k@avench.com>
2025-09-17 17:38:02 +0530

    Add Operator.java file

---
+++ src/Basics/operators.java
@@ -0,0 +1,38 @@
+package Basics;
+
+public class operators {
+    static void main(String[] args) {
+        System.out.println("Operators and Variables Tutorials!!");
+//    Variables -> A variable is a container used to store values.
+        int age = 25;  //here age is variable
+        float price = 99.99f; //price is variable
+        char grade = 'A'; //grade is variable
+        boolean isPassed = true; //isPassed is variable
+        String name = "Abhishek"; //name is variable
+
+        //operators -> Opertors are special symbols in java that perform operations on variables and values . they are fundamental to programming logic.
+
+//        1.Arithmetic Operators
+//        2.Relational Operators
+//        3.Logical Operators
+//        4.Assignment Operators
+//        5.unary operators
+//        6.Bitwise operators
+//        7.Ternary Operators
+
+//        ######      Arithmetic Operators   #######
+//        1.Addition  -> (+)
+//        2.subtraction -> (-)
+//        3.Multiplication -> (*)
+//        4.Division  -> (/)
+//        5.Modulus(Remainder) -> (%)
+
+        int a = 100 ;
+        int b = 25;
+        System.out.println("Addition: " + (a+b));
+        System.out.println("Subtraction: " +(a-b));
+        System.out.println("Multiplication: " +(a*b));
+        System.out.println("Division: " +(a/b));
+        System.out.println("Modules: " +(a%b));
+    }
+}
