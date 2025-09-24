7806459d9ed6a3e605afafd2412367b010ad107f
Abhishek Kumar <144894858+imabhi165@users.noreply.github.com>
2025-09-24 10:12:20 +0000

    updated small changes in String file.

--- src/Strings/StringBasics.java
+++ src/Strings/StringBasics.java
@@ -38,18 +38,43 @@ public class StringBasics {
          */
         //4. indexOf() and compareTo()
         //-----------------------------
+        System.out.println("Enter String: ");
         String str4 = sc.nextLine();
         int size = str4.length();
-        String str5 = "Hello!!";
+        String str5 = "Bikash";
         System.out.println("String Length: " + size);
         System.out.println(str4.indexOf('e'));
         System.out.println(str4.indexOf('l'));
         System.out.println(str4.indexOf('o'));
         System.out.println(str4.indexOf('z')); //if character not found then it will
+        System.out.println("From here CompareTo() method Started....!!");
         System.out.println(str4.compareTo(str5));
         
+        /*
+        * Important String Methods
+        * str.length();
+        * str.charAt();
+        * str.toUpperCase();
+        * str.toLowerCase();
+        * str.concat("Msg");
+        * str.contains("Msg");
+        * str.startsWith("java");
+        * str.endWith("ing");
+        * str.replace("Java", "C++");
+        * str.substring(5);
         
-
+        
+        String str = "Java Programming";
+         System.out.println(str.length());
+         System.out.println(str.charAt(3));
+         System.out.println(str.toUpperCase());
+         System.out.println(str.toLowerCase());
+         System.out.println(str.contains("gram"));
+         System.out.println(str.startsWith("java"));
+         System.out.println(str.endsWith("ing"));
+         System.out.println(str.replace("Java", "C++"));
+         System.out.println(str.substring(5));
+ */
 
     }
 }
