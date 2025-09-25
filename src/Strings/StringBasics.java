b897d6499eb7e04d06b3ad3410efc7307b35bec5
Abhishek Kumar <144894858+imabhi165@users.noreply.github.com>
2025-09-25 09:18:46 +0000

    Diff between StringBuilder and StringBuffer.

--- src/Strings/StringBasics.java
+++ src/Strings/StringBasics.java
@@ -35,7 +35,7 @@ public class StringBasics {
         System.out.println(str3.charAt(3));
         System.out.println(str3.charAt(4));
         //-------------------------------------------
-         */
+       
         //4. indexOf() and compareTo()
         //-----------------------------
         System.out.println("Enter String: ");
@@ -49,7 +49,7 @@ public class StringBasics {
         System.out.println(str4.indexOf('z')); //if character not found then it will
         System.out.println("From here CompareTo() method Started....!!");
         System.out.println(str4.compareTo(str5));
-
+  */
         /*
         * Important String Methods
         * str.length();
@@ -62,7 +62,7 @@ public class StringBasics {
         * str.endWith("ing");
         * str.replace("Java", "C++");
         * str.substring(5);
-         */
+         
         String str = "Java Programming";
         System.out.println(str.length());
         System.out.println(str.charAt(3));
@@ -73,6 +73,41 @@ public class StringBasics {
         System.out.println(str.endsWith("ing"));
         System.out.println(str.replace("Java", "C++"));
         System.out.println(str.substring(5));
+*/
+
+        // StringBuilder in Java -> Used to modify strings without creating new Objects.
+        // it is mutable
+        // faster than string in loops
+        // Not thread-safe (means not safe for multithreaded apps)
+        /* 
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" World");
+        sb.insert(5,"Java");
+        sb.delete(0, 5);
+        System.out.println(sb);
+        */
+        /*
+         * Important StringBuilder Methods:
+         * sb.append("text");  //Add at the End
+         * sb.insert(2,"x"); //insert at position
+         * sb.delete(1,3)  //removechars from index 1 to 2
+         * sb.reverse();  //reverse the string
+         * sb.replace(0,2, "Hi"); //replace chars from 0-1
+         */
+
+
+        //  3.StringBuffer in Java
+        // same as StringBuilder 
+        // but is is thread-safe
+        // Slightly slower than StringBuilder
+        // Example
+        StringBuffer sb = new StringBuffer("Hi");
+        sb.append(" Everyone");
+        System.out.println(sb);
+
+
+
+
 
     }
 }
