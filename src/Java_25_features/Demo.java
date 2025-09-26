f9dfef69f51b350fd76d83e38d1963eaee77e433
imabhi165 <abhi7808060981@gmail.com>
2025-09-27 01:33:33 +0530

    Added Researched Java 25 LTS update.

---
+++ src/Java_25_features/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello Java 25 LTS version!!!");
+    }
+
+    // void main{
+    //     IO.println("hello New features!!");
+    // }
+}
